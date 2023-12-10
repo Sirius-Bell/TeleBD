c594ef89cc368aaacbe263d8015b8d1b210473c8
Sirius Bell <vlad10game@gmail.com>
2023-12-10 16:30:45 +0300

    Add NoReturn to main

--- TeleBD/main.py
+++ TeleBD/main.py
@@ -5,20 +5,14 @@
 # Python 3.12
 
 import asyncio
+from typing import NoReturn
 
-from aiogram import Bot
-from aiogram import Dispatcher
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-
-from config import logger, config
+from config import logger, bot, dp
 from TeleBD.tg.routers.handler_options import router
 from TeleBD.tg.routers.common import common_router
 
 
-async def main():
-    bot = Bot(config.token.get_secret_value(), parse_mode=ParseMode.HTML)
-    dp = Dispatcher(storage=MemoryStorage())
+async def main() -> NoReturn:
     dp.include_router(common_router)
     dp.include_router(router)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
