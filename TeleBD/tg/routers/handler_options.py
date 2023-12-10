441d8d2094c10561e6bb9b86515b90a81f5fc781
Sirius Bell <vlad10game@gmail.com>
2023-12-10 16:34:02 +0300

    Add callback answers

--- TeleBD/tg/routers/handler_options.py
+++ TeleBD/tg/routers/handler_options.py
@@ -9,7 +9,9 @@ from aiogram.filters import StateFilter, Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from loguru import logger
-from tg.states.state_add import AddOption
+from TeleBD.tg.states.state_add import AddOption
+from TeleBD.tg.keyboards.kb import cancel
+from TeleBD.config import db
 
 router = Router()
 
@@ -18,28 +20,29 @@ router = Router()
 @router.callback_query(F.data == "add_option")
 async def add_option_handler(callback: CallbackQuery, state: FSMContext):
     logger.info("Catch add callback")
-    await callback.answer(text="Выберите имя услуги: ", show_alert=False)
+    await callback.message.answer(text="Выберите имя услуги", reply_markup=cancel)
     await state.set_state(AddOption.add_name_services)
+    await callback.answer(text="")
 
 
 @router.message(AddOption.add_name_services)
 async def add_name_service(msg: Message, state: FSMContext):
     await state.update_data(option_name=msg.text)
-    await msg.answer(text="Далее выберите имя картинки(с расширением): ")
+    await msg.answer(text="Далее выберите имя картинки(с расширением): ", reply_markup=cancel)
     await state.set_state(AddOption.add_img_name)
 
 
 @router.message(AddOption.add_img_name)
 async def add_img_name(msg: Message, state: FSMContext):
     await state.update_data(img_name=msg.text)
-    await msg.answer(text="Далее выберите описание услуги:")
+    await msg.answer(text="Далее выберите описание услуги:", reply_markup=cancel)
     await state.set_state(AddOption.add_description_services)
 
 
 @router.message(AddOption.add_description_services)
 async def add_description(msg: Message, state: FSMContext):
     await state.update_data(description=msg.text)
-    await msg.answer("Остался последний шаг: напишите цену: ")
+    await msg.answer("Остался последний шаг: цена услуги: ", reply_markup=cancel)
     await state.set_state(AddOption.add_price_services)
 
 
@@ -48,6 +51,4 @@ async def add_price(msg: Message, state: FSMContext):
     logger.info("Updating db...")
     await state.update_data(price=msg.text)
     await msg.answer("Заносим в базу данных.")
-    # data = await state.get_data()
-    # await database.add(data)
     await state.clear()
